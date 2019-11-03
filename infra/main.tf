4f568af7fc9c03b9991b5bdbd9e337244a53b8b9
pierreg256 <pierre.gilot@me.com>
2019-11-03 11:27:53 +0100

    functions releases container

--- infra/main.tf
+++ infra/main.tf
@@ -27,6 +27,13 @@ resource "azurerm_storage_account" "main" {
   tags                     = "${local.tags}"
 }
 
+resource "azurerm_storage_container" "apps" {
+  name                  = "${var.application}-functions"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  storage_account_name  = "${azurerm_storage_account.main.name}"
+  container_access_type = "private"
+}
+
 resource "azurerm_app_service_plan" "main" {
   name                = "${local.resource_prefix}-functions-sp"
   location            = "${azurerm_resource_group.main.location}"
